1ebc139d3b44003ba598a81e1b936d002ca42ea4
Iam-sree <iamsreeganesh87@gmail.com>
2024-12-28 09:27:17 +0530

    Add files via upload

---
+++ db/socializer.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 27, 2023 at 02:49 PM
+-- Server version: 5.7.39
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `socializer`
+
+use socializer;
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_content` varchar(255) NOT NULL,
+  `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment_content`, `comment_date`) VALUES
+(22, 26, 12, 'Heya Captain', '2023-03-27 14:46:34'),
+(23, 27, 11, 'Welcome Zoro', '2023-03-27 14:47:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `friend_id` int(11) NOT NULL,
+  `my_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `chat_active` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`friend_id`, `my_id`, `user_id`, `chat_active`) VALUES
+(13, 11, 12, 'YES'),
+(14, 12, 11, 'YES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `like_count` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `post_id`, `user_id`, `like_count`) VALUES
+(10, 26, 11, 1),
+(11, 26, 12, 1),
+(12, 27, 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `my_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message_content` varchar(255) NOT NULL,
+  `message_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `my_id`, `user_id`, `message_content`, `message_date`) VALUES
+(25, 11, 12, 'Hello', '2023-03-27 14:47:20'),
+(26, 11, 12, 'Hello', '2023-03-27 14:47:32'),
+(27, 12, 11, 'hello', '2023-03-27 14:47:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_content` varchar(255) NOT NULL,
+  `upload_image` varchar(255) NOT NULL,
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `user_id`, `post_content`, `upload_image`, `post_date`) VALUES
+(26, 11, 'Hello I am Monkey D. Luffy, I will become the King of The Pirates', 'post/post_luffy_230327024215_Aesthetic-Luffy-Wallpaper.jpg', '2023-03-27 14:42:15'),
+(27, 12, 'Roronoa Zoro, I will become the Greatest Swordsman', 'post/post_zoro_230327024615_one-piece-roronoa-zoro-hd-wallpaper-preview.jpg', '2023-03-27 14:46:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `country` varchar(15) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `dob` date NOT NULL,
+  `describe_user` text NOT NULL,
+  `relationship` text NOT NULL,
+  `user_image` varchar(255) NOT NULL,
+  `user_cover` varchar(255) NOT NULL,
+  `user_registration_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` text NOT NULL,
+  `posts` text NOT NULL,
+  `recovery_account` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `fullname`, `username`, `email`, `password`, `country`, `gender`, `dob`, `describe_user`, `relationship`, `user_image`, `user_cover`, `user_registration_date`, `status`, `posts`, `recovery_account`) VALUES
+(11, 'Monkey D. Luffy', 'luffy', 'Luffy@gamil.com', 'e7e99f843ebdabec28d00203c8155085', 'Japan', 'Male', '1999-08-17', 'Hey! Socializer', '...', 'users/profile_luffy_230327024104_Aesthetic-Luffy-Wallpaper.jpg', 'cover/cover_luffy_230327024126_one-piece-title-cover-yuha9ifvg728ddf6.jpg', '2023-03-27 14:38:25', 'verified', 'yes', '...'),
+(12, 'Roronoa Zoro', 'zoro', 'Zoro@gamil.com', 'abed451eb4cb1fb8ba4185adb2c1247b', 'Japan', 'Male', '1998-02-11', 'Hey! Socializer', '...', 'users/profile_zoro_230327024358_one-piece-roronoa-zoro-hd-wallpaper-preview.jpg', 'cover/cover_zoro_230327024426_one-piece-title-cover-yuha9ifvg728ddf6.jpg', '2023-03-27 14:43:32', 'verified', 'yes', '...');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`friend_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`,`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `friend_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
